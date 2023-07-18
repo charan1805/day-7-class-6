459b4fd363fee2dab7795e9319b3b7e348ddc8ac
charan1805 <sreecharanmudhiraj@gmail.com>
2023-07-18 14:06:19 +0530

    First Commit

---
+++ class 6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_6
+{
+   
+
+namespace DirectoryExample
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                
+
+
+
+                string fPath;
+                Console.WriteLine("Input Directory Path: ");
+                fPath = Console.ReadLine();
+                DirectoryInfo directory = new DirectoryInfo(fPath);
+                foreach (FileInfo file in directory.GetFiles())
+                {
+                    Console.WriteLine(" File Full Name: \t" + file.FullName);
+                    Console.WriteLine("\tFile Length: " + file.Length);
+                    Console.WriteLine("\tFile Creation Time: " + file.CreationTime);
+                    Console.WriteLine("\n");
+                }
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
+    
+
